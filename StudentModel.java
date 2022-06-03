2a2b2580c494abadb453b5e15b1dd25c8fd4941e
azaimirul <81767248+azaimirul@users.noreply.github.com>
2022-06-03 17:39:39 +0800

    Create StudentModel.java

---
+++ StudentModel.java
@@ -0,0 +1,49 @@
+
+public class StudentModel {
+	
+	private int id;
+	private String name;
+	private double mathMark;
+	private double scienceMark;
+	private String department;
+	
+	public int getid() {
+		return id;
+	}
+	
+	public void setid(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getMathMark() {
+		return mathMark;
+	}
+	
+	public void setMathMark(double mathMark) {
+		this.mathMark = mathMark;
+	}
+	
+	public double getScienceMark() {
+		return scienceMark;
+	}
+	
+	public void setScienceMark(double scienceMark) {
+		this.scienceMark = scienceMark;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+}
